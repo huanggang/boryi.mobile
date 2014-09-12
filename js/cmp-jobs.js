d37cc9fc9a8feccdfba23e1d533b4f2b635bdebc
boryicode <boryicode@gmail.com>
2014-09-12 16:30:21 +0800

    untested code

---
+++ js/cmp-jobs.js
@@ -0,0 +1,541 @@
+$(document).ready(function(){
+    //fill the province first,setSelections defined in mobilecommon.js
+    setSelections(provinces, 'province', 10000);
+
+    var cityCache = {}; 
+    
+    $('#province').change(function(event) {
+	var provinceid = $("#province option:selected").val();
+	if (provinceid){
+	    if (cityCache.hasOwnProperty(provinceid)){
+	        setSelections(cityCache[provinceid], 'city');
+	    } else {
+	        setCities(provinceid, setCityOptions);
+	    }
+	}
+    });
+
+    var setCityOptions = function (cities){
+        setSelections(cities, 'city');
+    }
+
+    ///  provinceid - the id of the province
+    ///  callback   - the method which needs city json as input  
+    function setCities(provinceid, callback, async) { 
+        if (provinceid == null){
+            return;
+        }
+        if (provinceid == 10000){
+  	    // no cities if all country has been selected
+  	    setSelections([], 'city');
+            return;
+        }
+        
+        cityCache[provinceid] = loadCitiesByProvinceId(provinceid);
+        callback(cityCache[provinceid]);
+    }
+
+    /// get the workplace according to the country, province and city
+    function getWorkPlace(){
+        var city = $("#city option:selected").val();
+        if (city == undefined) { city=''; }
+        if (city.length > 0){
+            return '1' + city + '00000000000';
+        } else {
+            var province = $("#province option:selected").val();
+            if (province.length == 5){
+                return '1000000000000000';
+            } else {
+                return '1' + province + '00000000000';
+            }
+        }
+    }
+
+    var page = []; // the page option
+    page.c = {};   // initialize the company cache
+    page.j = {};   // initialize the job cache
+
+    var base = 'http://www.boryi.com:8080/SearchJobs/';
+
+    var searchJob = function(form){ 
+        var targetUrl = base + 'jobs?';
+        targetUrl += 's1='  + getWorkPlace();                       // 工作地点
+        
+        var cmptype = getSelected('cmp-type');
+        if(cmptype>0){
+            targetUrl += '&s2=' + cmptype;               // 公司性质
+        }
+
+        var keyword = $.trim($("#keyword").val());
+        if (keyword.length > 0){
+            targetUrl += '&s3=' + keyword;                            // 关键字
+        }
+        else
+        {
+            return; //don't send request if the keyword was not inputed.
+        }
+        
+        var jobtype = getSelected('job-type');
+        if (jobtype.length > 0){
+            targetUrl += '&s4=' + jobtype;                            // 工作类型
+        }
+
+        $.ajax({
+            url: targetUrl,
+            dataType: "jsonp", 
+            jsonpCallback: "_jobs", 
+            cache: true,
+            timeout: 5000, 
+        }).done(function(d) {
+            if (d.t == 0){
+                alert('没有找到符合条件的结果，请修改查询条件重试');          
+            } else {
+                // clear the list first
+                $('ul.list').empty();
+                // set global trackers
+                page.currentp = 1;  // initialize the current page tracker mark
+                page.currentq = 0;  // initialize the current query id mark
+                page.total = 0;     // initialize the total result mark
+                page.c = {};        // initialize the company cache
+                page.j = {};        // initialize the job cache
+                
+                // enable the list and detail tab
+                $('#tab-list, #tab-detail').click(tabHandler);
+
+                // display results in the list tab 
+                showJobs(d);          
+            }
+        }).fail(function(xhr, status, msg) {
+            alert('网络不太给力，请重试');
+        });
+    };
+
+    /// display searching results 
+    function showJobs(json){
+        $('#tab-list').trigger('click');
+        
+        if (page.currentq == 0){
+            // to get the query id if there exists
+            page.currentq = json['q'] || 0;
+        } 
+
+        if (page.total == 0){
+            // to get the total if there exists
+            page.total = json['t'] || 0;
+        } 
+
+        if ((page.total - 1) / 20 <= page.currentp++){
+            $('#more').hide();
+        } else {
+            $('#more').show();
+        }
+
+        var companies = json['c'];
+        var jobs = json['j'];
+
+        for (var i = 0; i <= companies.length - 1; i++) {
+            var company = companies[i];
+
+            var cid = company.id.toString();
+            // u cannot directly use company.id as the key of cached data,
+            // we must conver it to a string first, like this
+
+            // check if this company has alreay been cached
+            var cachedCmp = $(page.c).data(cid);
+            if (cachedCmp === undefined){
+                // new company cached
+                $(page.c).data(cid, company);
+            }
+        }
+
+        var li = $('<li />').addClass('list-item');
+        var fc = $('<div />').addClass('fc');
+        var fbc = fc.addClass('fb');
+        var fr = $('<div />').addClass('fr');
+        var fl = $('<div />').addClass('fl');
+        var cmp = $('<div />').addClass('fl fb w75');
+
+        var title_div = $('<div />').addClass('list-title fb');
+
+        for (var i = 0; i <= jobs.length - 1; i++) {
+            var job = jobs[i];
+            var jid = job['cid'] + '-' + job['jid'];
+            // check if this job has alreay been cached
+            var cachedJob = $(page.j).data(jid);
+            if (cachedJob === undefined){
+                // new company cached
+                $(page.j).data(jid, job);
+            }
+
+            var title = title_div.clone().append(job['ttl']).wrap('<div />');
+
+            var list = li.clone();
+            list.append(title);
+            list.append(fc.clone()
+                        .append(cmp.clone().append(
+                            map_id_attr(companies, job['cid'], 'nm'))
+                               )
+                        .append(fr.clone().append(job['rfr']))
+                       );
+
+            list.append(fbc.clone()
+                        .append(fl.clone().append(showRange(job['slr'], '元以上', '元以下', '元', '~')))
+                        .append(fr.clone().append('消息来源:' + sources[job['src'][0]['sid'] - 1]))
+                       ).append(fc.clone());
+            
+            $('ul.list').append(list.attr({'v':jid}));
+        }
+
+        $('.list .list-item').click(function(){
+            $(this).addClass('viewed');
+
+            $('#tab-detail').trigger('click');
+            var v = $(this).addClass('viewed').attr('v').split('-');
+
+            showJobDetails(v[0], v[1]);
+        })
+    }
+
+    /// display job details
+    ///
+    /// cid: company id
+    /// jid: job id
+    function showJobDetails(cid, jid){
+        var company = $(page.c).data(cid); 
+        var job = $(page.j).data(cid + '-' + jid); 
+
+        $('#company').html(company.nm); 
+        $('#title').html(job.ttl); 
+        $('#refreshdate').html(job.rfr); 
+        $('#source').html(sources[job.src[0]['sid'] - 1]); 
+
+        $('#apply-source').click(function(){ 
+            location.href = job.src[0]['url']; 
+        }); 
+        
+        $('#salary-str').html(showRange(job.slr, '元以上', '元以下', '元', '~'));
+
+        var edu = job.edu;
+        var xpr = job.xpr;
+        var gnd = job.gnd;
+        var age = job.age;
+        var hgh = job.hgh;
+        var lng = job.lng;
+        
+        var bnf = job.bnf;
+        var ftr = job.ftr;
+
+        var rqs = '';
+        var bns = '';
+        
+        if (edu){
+            rqs += educations[edu - 1] + ' &middot; ';
+        }
+        if (xpr){
+            rqs += xpr + '年以上工作经验 &middot; ';
+        }
+        if (gnd){
+            rqs += genders[gnd] + '&middot; ';
+        }
+        if (age){
+            var age_str = '';
+            var male_age = showRange(age.slice(0, 2), '岁以上', '岁以下', '岁', '-');
+            var femail_age = showRange(age.slice(2), '岁以上', '岁以下', '岁', '-');
+            if (male_age){
+                age_str += '男：' + male_age;  
+            }
+            if (femail_age){
+                if (male_age){
+                    age_str += ', ';
+                }
+                age_str += '女：' + femail_age;  
+            }
+            
+            rqs += age_str + '&middot; ';
+        }
+
+        if (hgh){
+            var hgh_str = '';
+            hgh_str += '男：' + showRange(hgh.slice(0, 2), 'CM以上', 'CM以下', 'CM', '-');
+            hgh_str += ', 女：' + showRange(hgh.slice(2), 'CM以上', 'CM以下', 'CM', '-');
+            rqs += hgh_str + '&middot; ';
+        }
+
+
+        if (lng){
+            var lng_str = '';
+            for (var i = 0; i < lng.length; i++) {
+                var lng_id = lng[i];
+                var lng_name = '';
+
+                if (lng_id % 1000 == 0){
+                    lng_name = map_id_attr(languages, lng_id, 'n');
+                } else {
+                    var index = parseInt(lng_id / 1000) - 1;
+                    console.log('语言');
+                    console.log(index);
+                    console.log(languages[index]);
+                    lng_name = map_id_attr(languages[parseInt(lng_id / 1000) - 1].s, lng_id, 'n');
+                }
+                lng_str += lng_name + ', ';  
+            }
+            if (lng_str){
+                // remove the last ',' and ' '
+                lng_str = lng_str.substring(0, lng_str.length - 2);
+            }
+            rqs += lng_str + '&middot; ';
+        }
+
+        if (bnf){
+            var bnf_str = '';
+            for (var i = 0; i < bnf.length; i++) {
+                bnf_str += benefits[bnf[i] - 1] + ', ';
+            }
+            if (bnf_str){
+                // remove the last ',' and ' '
+                bnf_str = bnf_str.substring(0, bnf_str.length - 2);
+            }
+            bns += bnf_str + '&middot; ';
+        }
+
+        if (ftr){
+            var ftr_str = '';
+            for (var i = 0; i < ftr.length; i++) {
+                ftr_str += features[ftr[i] - 1] + ', ';
+            }
+            if (ftr_str){
+                // remove the last ',' and ' '
+                ftr_str = ftr_str.substring(0, ftr_str.length - 2);
+            }
+            bns += ftr + '&middot; ';
+        }
+
+        if (rqs.length){
+            // remove the last extra '&middot; '
+            rqs = rqs.substring(0, rqs.length - 9);
+        }
+        if (bns.length){
+            // remove the last extra '&middot; '
+            bns = bns.substring(0, bns.length - 9);
+        }
+
+        $('#requirement-list').html(rqs);
+        $('#benefit-list').html(bns);
+
+        console.log('...this is job...');
+        console.log(job);
+        
+        if (company.ty){
+            var cmpType = companyTypes[(company.ty - 1)];
+            $('#cmp-type-str').html(cmpType);  
+        }
+
+        var size = company.sz;
+        var size_str = '';
+        if (size){
+            $('#cmp-size-str').html(showRange(size, '人以上', '人以下', '人', '-'));
+        }
+
+        var loc = company['lc'].toString();
+        var cn = loc.substr(0, 1);
+        var province = loc.substr(1, 2);
+        var city = loc.substr(1, 4);
+
+        // set the company's location 
+        if (!cn){
+            $('#cmp-location').html('国外');
+        } else {
+            // in CN
+            var provinceid = province * 100;
+            var prov_str = map_id_attr(provinces, provinceid);
+            
+            var city_str = '';
+            var setCmpLocation = function(cities){
+                city_str = map_id_attr(cities, city);
+                if (city_str){
+                    city_str = '-' + city_str;
+                }
+                $('#cmp-location').html(prov_str + city_str);
+            }
+
+            if (cityCache.hasOwnProperty(provinceid)){
+                setCmpLocation(cityCache[provinceid]);
+            } else {
+                setCities(provinceid, setCmpLocation);
+            }
+
+
+
+
+
+            //$('#cmp-location').html(province);
+        }
+
+
+        var jobUrl = base + 'job?c=' + cid + '&j=' + jid;
+
+        $.ajax({
+            url: jobUrl,
+            dataType: "jsonp", 
+            jsonpCallback: "_job", 
+            cache: true,
+            timeout: 10000,
+        }).done(function(j) {
+            $('#postdate').html(j['ffd']); 
+
+            if(j['dsc']){
+                $('#description').html(j['dsc']);
+                $('#description-block').show();
+            } else {
+                $('#description-block').hide();
+            }
+
+            if(j['rqr']){
+                $('#requirement').html(j['rqr']);  
+                $('#requirement-block').show();
+            } else {
+                $('#requirement-block').hide();
+            }
+
+            if(j['eml']){
+                var email = j['eml']; 
+                $('#email').html(email).attr('href', 'mailto:' + email);   
+                $('#email-block').show();
+            } else {
+                $('#email-block').hide();
+            }
+            
+            if (j['cnt']){
+                $('#contact').html(j['cnt']); 
+                $('#contact-block').show();   
+            } else {
+                $('#contact-block').hide();
+            }
+            
+            if (j['phn']){
+                var phones = j['phn'].split(',');
+                var phones_str = '';
+                for (var i = phones.length - 1; i >= 0; i--) { 
+                    phones_str += '<a id="phone' + i + '" href="tel:' + $.trim(phones[i].replace(/\s+-\(\)/g, '')) + '" >' + phones[i] + '[<span class="call">拨打</span>]<br />';
+                };
+                $('#phone').html(phones_str);  
+                $('#phone-block').show();
+            }
+
+            if (j['mbl']){
+                var mobiles = j['mbl'].split(',');
+                var mobiles_str = '';
+                for (var i = mobiles.length - 1; i >= 0; i--) {
+                    mobiles_str += '<a id="mobile' + i + '" href="tel:' + $.trim(mobiles[i].replace(/\s+-\(\)/g, '')) + '" >' + mobiles[i] + '[<span class="call">拨打</span>]<br />';
+                };
+                $('#cell').html(mobiles_str);  
+                $('#cell-block').show();
+            }
+        }).fail(function(xhr, status, msg) { 
+            alert('网络不太给力，拿不到工作信息，请重试' + msg + status); 
+        }); 
+
+    }
+
+    // load another 20 results if there exists 
+    $('#more').click(function(){
+        var targetUrl = base + 'jobs?q=' + page.currentq + '&p=' + page.currentp;
+
+        $.ajax({
+            url: targetUrl,
+            dataType: "jsonp", 
+            jsonpCallback: "jcb", 
+            cache: true,
+            timeout: 10000,
+        }).done(function(d) {
+            // display results in the 2nd tab
+            showJobs(d); 
+        }).fail(function(xhr, status, msg) { 
+            alert('网络不太给力，请重试'); 
+        }); 
+    });
+
+    // the form validator
+    $("#search-form").validate({
+        errorPlacement: errPlace,
+        submitHandler: searchJob,
+        rules: { 
+            keyword: { 
+          	maxlength: 40, 
+            }, 
+            salary: { 
+        	number: true,
+        	min: 500, 
+        	max: 999999, 
+            }, 
+            experience: {
+        	number: true,
+        	min:0,
+        	max:60,
+            },
+            age: {
+        	number: true,
+        	min:16,
+        	max:80,
+            },
+            height: {
+        	number: true,
+        	min:70,
+        	max:250,
+            },
+            'cmp-size-low': { 
+        	number: true,
+        	min: 10, 
+            }, 
+            'cmp-size-high': { 
+        	number: true,
+        	min: 10, 
+            }, 
+            'keyword-job': {
+		keywordtype: true,
+	    },
+            'keyword-cmp': {
+		keywordtype: true,
+	    },
+        }, 
+        messages: { 
+            keyword: { 
+                maxlength: "关键字不能超过40个字符", 
+            }, 
+            salary: { 
+                number: '请输入数字',
+                min: "月薪不能小于500元", 
+                max: "月薪不能大于999999元", 
+            }, 
+            experience: {
+                number: '请输入数字',
+                min: "工作经验不能小于0年", 
+                max: "工作经验不能大于60年", 
+            },
+            age: {
+                number: '请输入数字',
+                min: "年龄不能小于16", 
+                max: "年龄不能大于80", 
+            },
+            height: {
+                number: '请输入数字',
+                min: "身高不能低于70cm", 
+                max: "身高不能超过250cm", 
+            },
+            'cmp-size-low': { 
+                number: '请输入数字',
+                min: "公司规模不能少于10人", 
+            },
+            'cmp-size-high': { 
+                number: '请输入数字',
+                min: "公司规模不能少于10人", 
+            },
+            'keyword-job': {
+                keywordtype: '职位名公司名至少选择一个',
+            },
+            'keyword-cmp': {
+                keywordtype: '职位名公司名至少选择一个',
+            },
+        },
+    });
+});
--- js/mobilecommon.js
+++ js/mobilecommon.js
@@ -4,66 +4,80 @@
 ///  target - id of the select input 
 ///  value  - the default value
 function setSelections(json, target, value){
-	var len = json.length;
-	var target = $('#' + target);
-	if (len == 0){
-		target.hide();
-	} else {
-	  target.show();
-	}
-	target.empty();//.append('<option value="-1">-</option>');
-	var option = $('<option/>');
-  	for(var j = 0; j < len; j++) {
-    	var op = option.clone();
-    	op.attr('value', json[j].id); // fixed typo 
-    	if (value != undefined && value == json[j].id){
-        	op.attr('selected', 'selected');
-     	}
-    	op.append(json[j].name);
-    	target.append(op);
-  }
+    var len = json.length;
+    var target = $('#' + target);
+    if (len == 0){
+        target.hide();
+    } else {
+        target.show();
+    }
+    target.empty();//.append('<option value="-1">-</option>');
+    var option = $('<option/>');
+    for(var j = 0; j < len; j++) {
+        var op = option.clone();
+        op.attr('value', json[j].id); // fixed typo 
+        if (value != undefined && value == json[j].id){
+            op.attr('selected', 'selected');
+        }
+        op.append(json[j].name);
+        target.append(op);
+    }
 }
 
 /// get any json property according to 'id' property
 /// if not given, attr would be set to 'name'
 function map_id_attr(jsons, id, attr){
- 	if (typeof attr == "undefined"){
-   		attr = 'name';
- 	}
-	for (var i = 0; i < jsons.length; i++){
-		var j = jsons[i];
-		if (j.id == id) {
-			return j[attr];
-		}
-	}
-	return '';
+    if (typeof attr == "undefined"){
+        attr = 'name';
+    }
+    for (var i = 0; i < jsons.length; i++){
+        var j = jsons[i];
+        if (j.id == id) {
+            return j[attr];
+        }
+    }
+    return '';
 };
 
 /// get the value of the selected option
 function getSelected(select){
-	return $("#" + select + " option:selected").val();
+    return $("#" + select + " option:selected").val();
 }
 
 /// Default method which customs how error label displayed
 /// for form validation
 var errPlace = function(error, element) {
-  element.parent().append(error);
+    element.parent().append(error);
 }
 
 
 /// Display the range of a json , such as company size range
 /// and salary range, etc.
 function showRange(range, lowstr, highstr, unit, range_concat){
-	var result = '';
-	range = range || [];
-	if (range.length == 2){
-	  if (range[0] == null && range[1] > 0){
-	    return range[1] + highstr;
-	  } else if (range[0] > 0 && range[1] == null){
-	    return range[0] + lowstr;
-	  } else if (range[0] > 0 && range[1] > 0){
-	    return range[0] + range_concat + range[1] + unit;
-	  } 
-	}
-	return result;
+    var result = '';
+    range = range || [];
+    if (range.length == 2){
+        if (range[0] == null && range[1] > 0){
+            return range[1] + highstr;
+        } else if (range[0] > 0 && range[1] == null){
+            return range[0] + lowstr;
+        } else if (range[0] > 0 && range[1] > 0){
+            return range[0] + range_concat + range[1] + unit;
+        } 
+    }
+    return result;
+}
+
+//load the cities by provinceid
+function loadCitiesByProvinceId(provinceid,async){
+    var url = '/js/city/cities_' + provinceid + '.js';
+    var cities;
+    $.ajax({
+        dataType: "script",
+        cache: true,
+        url: url,
+        async: async,}).done(function(){
+            cities = eval('cities_' + provinceid);
+        });
+    return cities;
 }
